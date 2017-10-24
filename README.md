452e655cde6f93bf95d5c685412e330c659eead3
Christian Townsdin <ctownsdin@expedia.org>
2017-10-24 14:29:54 -0700

    commit completed files

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # graphQLexploration
-exploring graphQL
+test and learning graphQL
 
-npm install    
-npm run start    
+`$ npm run start`
+to start Express-GraphQL server on port 4000, with nodemon watching & reloading
 
-browse to: http://localhost:3000
+`$ json-server --watch db.json`
+to start JSON server on port 3000
+a fully restful service endpoint is launched from the db.json file.
+
+
+Fully working and understood...    
+GraphQL schema with Objects, resolve functions, queries, and mutations including edit and delete.
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 const expressGraphQL = require('express-graphql')
-const schema = require('./schema');
+const schema = require('./schema/schema')
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.use('/graphql', expressGraphQL({ 
+app.use('/graphql', expressGraphQL({
   schema,
-  graphiql: true 
-}));   // use dev tool: graphiql, to test graphQL
+  graphiql: true
+}))   // use dev tool: graphiql, to test graphQL
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000')
+app.listen(4000, function () {
+  console.log('Express-GraphQL listening on port 4000')
 })
